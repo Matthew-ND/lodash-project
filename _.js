4dd10f22b01ae41a97d4fee20f5500d730825982
Matthew Doucette <matthewdoucette@hotmail.com>
2024-01-12 16:53:17 -0500

    Add .inRange

--- _.js
+++ _.js
@@ -1,15 +1,33 @@
 
 const _ = {
+    //.clamp() takes three arguments: a number, a lower bound, and an upper bound
+    //Returns a number that is within the range or transforms it to fit within the range
     clamp(num, lowR, highR) {
-        if (num <= lowR) {
+        if (num < lowR) {
             return lowR;
-        } else if (num >= highR) {
+        } else if (num > highR) {
             return highR;
         } else {
             return num;
         }
+    }, // EXPERIMENT WITH MATH.MIN / MATH.MAX
+
+    //.inRange() takes three arguments: a number, a start value, and an end value.
+    //Returns true if number is within range, false if it is outside
+    inRange(num, lowR = 0, highR = num) {
+        if (lowR === highR) {
+            return false;
+        }
+        let low = lowR;
+        let high = highR;
+        if (low > high) {
+            high = lowR;
+            low = highR;
+        }
+        return num >= low && num < high;
     }
 };
+
 
 
 
